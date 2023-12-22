ec0fc902f5fde02a5b657321059072e5f4c8eee8
yasminetak <81821967+yasminetak@users.noreply.github.com>
2023-12-22 02:24:08 +0100

    New changes

--- BackEnd/post/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('posts/', views.PostView.as_view(), name= 'posts_list'),
-    #path('ocr_process/', views.PostView.as_view({'post': 'ocr_process'}), name='ocr_process')
-    #path('ocr_process/', views.PostView.as_view, name='ocr_process'),
-]
--- Smartpack-Dashboard/src/components/logo.js
+++
@@ -1,7 +0,0 @@
-import { useTheme } from '@mui/material/styles';
-import React from 'react';
-
-export const Logo = ({ src }) => {
-  return <img src={src} alt="Logo" style={{ height: '100%', width: '100%' }} />;
-};
-
